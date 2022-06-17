55166c21f7939c04f1c5bbd11031ce038f28232c
KondratVladymyr <105788566+KondratVladymyr@users.noreply.github.com>
2022-06-17 10:46:26 +0300

    conc1

---
+++ Conc.java
@@ -0,0 +1,9 @@
+public class Conc {
+    public static void main(String[] args){
+    System.out.println(concatStr("ABCD","FGH"));
+    }
+    static String concatStr(String a, String b){
+        return a+b;
+    }
+
+}
